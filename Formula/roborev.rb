d720a3fef19a98acfdaf41115a2693bf4dc07dcd
Wes McKinney <wesmckinn+git@gmail.com>
2026-01-24 12:58:09 -0500

    Update roborev to v0.15.2 with correct checksums

--- Formula/roborev.rb
+++ Formula/roborev.rb
@@ -1,28 +1,28 @@
 class Roborev < Formula
   desc "Automatic code review daemon for git commits using AI agents"
   homepage "https://roborev.io"
-  version "0.14.0"
+  version "0.15.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/roborev-dev/roborev/releases/download/v#{version}/roborev_#{version}_darwin_amd64.tar.gz"
-      sha256 "PLACEHOLDER_INTEL_SHA256"
+      sha256 "fa4af9610b86fa89c6148055d302945aabf3657abadd4e388254aa07b93faba0"
     end
     if Hardware::CPU.arm?
       url "https://github.com/roborev-dev/roborev/releases/download/v#{version}/roborev_#{version}_darwin_arm64.tar.gz"
-      sha256 "PLACEHOLDER_ARM64_SHA256"
+      sha256 "0e03f25abfcfd5b842df2951297041e5b7bf1eaa619451d64d52aa2137df603f"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/roborev-dev/roborev/releases/download/v#{version}/roborev_#{version}_linux_amd64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_INTEL_SHA256"
+      sha256 "1c4c7337358527f32fea17565258b52b40905b1c9a795d1329ad7c59cc545910"
     end
     if Hardware::CPU.arm?
       url "https://github.com/roborev-dev/roborev/releases/download/v#{version}/roborev_#{version}_linux_arm64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
+      sha256 "490dcf10585dfa4d5f949dbc11422e5cb6051c70e2a35da5ebf18f00a51b5b4a"
     end
   end
 
